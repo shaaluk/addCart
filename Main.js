7fbcc9eaf34c2c824dc5665fbe4ac860eeac8576
shaaluk <112568360+shaaluk@users.noreply.github.com>
2023-09-10 22:31:23 +0530

    Main.js

--- Main.js
+++ Main.js
@@ -1,23 +1,25 @@
 import React from "react";
 import Product from "./Product";
 
-
- function Main(props){
-    const {products, setBasketData,basketData} = props;
-  return(
+function Main(props) {
+  const { products, setBasketData, basketData } = props;
+  return (
     <div className="block col-2">
       <h2>Products</h2>
 
       <div className="row">
-        
-        {products.map((product)=>(
-        // <div>{product.name}{product.content}{product.image}</div>
-        <Product key={product?.id} product={product} basketData={basketData} setBasketData={setBasketData}/>
-          
+        {products.map((product) => (
+          // <div>{product.name}{product.content}{product.image}</div>
+          <Product
+            key={product?.id}
+            product={product}
+            basketData={basketData}
+            setBasketData={setBasketData}
+          />
         ))}
       </div>
     </div>
-  )
- }
+  );
+}
 
- export default Main;
+export default Main;
