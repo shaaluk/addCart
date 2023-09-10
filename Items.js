9f3c1d5f20fe60684c061544ff3118d0306fa1e5
shaaluk <112568360+shaaluk@users.noreply.github.com>
2023-09-10 22:29:41 +0530

    Items.js

--- Items.js
+++ Items.js
@@ -1,65 +1,85 @@
 import { useState } from "react";
 
 const Items = (props) => {
-    const { image, name,price,onDelete, qty} = props;
+  const { image, name, price, onDelete, qty, setBasketData, idx, basketData } =
+    props;
 
-    const [quantity, setQuantity] = useState(qty);
+  const [quantity, setQuantity] = useState(qty);
 
-    const handleInc = () => {
-        setQuantity(quantity + 1);
-    };
+  const handleInc = () => {
+    let newbasketData = basketData?.map((data, id) => {
+      if (idx == id) {
+        return {
+          ...data,
+          total: data?.price * quantity + data?.price,
+        };
+      } else {
+        return {
+          ...data,
+        };
+      }
+    });
+    setBasketData(newbasketData);
+    console.log("newbasketData", newbasketData);
+    setQuantity(quantity + 1);
+  };
 
-    const handleDec = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
+  const handleDec = () => {
+    if (quantity > 1) {
+      let newbasketData = basketData?.map((data, id) => {
+        if (idx == id) {
+          return {
+            ...data,
+            total: data?.price * (quantity - 1),
+          };
+        } else {
+          return {
+            ...data,
+          };
         }
-        else if (quantity === 1) {
-            onDelete();
-        }
+      });
+      setBasketData(newbasketData);
+      setQuantity(quantity - 1);
+    } else if (quantity === 1) {
+      onDelete();
     }
-    
-    
+  };
 
-    return (
-        <>
-
-            <div className="items-info">
-
-                <div className="order-child">
-                    <div id="newOrder">
-                    <div className="imageBox">
-                        <img className="newImage" src={image} alt="hi" />
-                    </div>
-
-                    <div className="title1">
-                        <h3 className="nameItem">{name}</h3>
-                        <button className="btn" onClick={handleInc}>+</button>
-                        <span>{quantity}</span>
-
-                        <button className="btn" onClick={handleDec}>-</button>
-
-                        <div class="dollarSign">${price*quantity}</div>
-                    </div>
-
-                    <div className="buttonBox">
-                        <button className="deleteBtn" onClick={onDelete}>Delete</button>
-                    </div>
-                    </div>
-
-                </div>
-
+  return (
+    <>
+      <div className="items-info">
+        <div className="order-child">
+          <div id="newOrder">
+            <div className="imageBox">
+              <img className="newImage" src={image} alt="hi" />
             </div>
 
-            <hr/>
-           
-        
-             
-        </>
-        
-    )
-}
+            <div className="title1">
+              <h3 className="nameItem">{name}</h3>
+              <button className="btn" onClick={handleInc}>
+                +
+              </button>
+              <span>{quantity}</span>
+
+              <button className="btn" onClick={handleDec}>
+                -
+              </button>
+
+              <div class="dollarSign">${price * quantity}</div>
+            </div>
+
+            <div className="buttonBox">
+              <button className="deleteBtn" onClick={onDelete}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
+};
 
 export default Items;
-
-
-
